f70421bae64d54e7202a8cb83fbc2b38c49c6f10
LukeRMiles <128188165+LukeRMiles@users.noreply.github.com>
2026-03-26 20:18:52 -0400

    Added Advisor model

---
+++ src/main/java/org/example/comp2800_sas/model/Advisor.java
@@ -0,0 +1,35 @@
+package org.example.comp2800_sas.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ADVISOR")
+public class Advisor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "advisor_id")
+    private Integer advisorId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    public Advisor() {
+
+    }
+
+    public Advisor(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
